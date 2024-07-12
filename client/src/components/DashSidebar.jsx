cda19dbf96ae42f4f9f32592d9f51aa4ff004496
Rachamv <rkutalian@gmail.com>
2024-07-12 03:40:05 +0100

    create all needed files

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DashSidebar() {
+  return (
+    <div>DashSidebar</div>
+  )
+}
+
+export default DashSidebar
---
+++ client/src/redux/theme/themeSlice.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function themeSlice() {
+  return (
+    <div>themeSlice</div>
+  )
+}
+
+export default themeSlice
